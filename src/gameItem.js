5fda0312d98db5e3c9258eaa24dd6a0db053d3e9
Cindy <cindytvn+git@gmail.com>
2021-01-25 21:04:26 +0100

    created moving methods + xw and yh in GameItem class

--- src/gameItem.js
+++ src/gameItem.js
@@ -5,47 +5,66 @@ class GameItem {
         this.y = y
         this.width = width
         this.height = heigth
-        this.xw = x + width
-        this.yh = y + height
-    }
-
+    };
+    // derivated value xw and xh insted of directly saving it
+    xw() {
+        return this.x + this.width;
+    };
+    yh() {
+        return this.y + this.heigth;
+    };
     cornerCheck(otherGameItem) {
         // check if the left upper coordinate (y,x) of other object lies ín the this.object
         if (this.x <= otherGameItem.x &&
-            this.xw >= otherGameItem.x &&
+            this.xw() >= otherGameItem.x &&
             this.y <= otherGameItem.y &&
-            this.yh >= otherGameItem.y) {
+            this.yh() >= otherGameItem.y) {
                 return true
-        }
+        };
         // check if the left down coordinate (x, yh) of other object lies ín the this.object
         if (this.x <= otherGameItem.x &&
-            this.xw >= otherGameItem.x &&
-            this.y <= otherGameItem.yh &&
-            this.yh >= otherGameItem.yh) {
+            this.xw() >= otherGameItem.x &&
+            this.y <= otherGameItem.yh() &&
+            this.yh() >= otherGameItem.yh()) {
                 return true
-        }
+        };
         // check if the right upper coordinate (xw, y) of other object lies ín the this.object
-        if (this.x <= otherGameItem.xw &&
-            this.xw >= otherGameItem.xw &&
+        if (this.x <= otherGameItem.xw() &&
+            this.xw() >= otherGameItem.xw() &&
             this.y <= otherGameItem.y &&
-            this.yh >= otherGameItem.y) {
+            this.yh() >= otherGameItem.y) {
                 return true
-        }
+        };
         // check if the right down coordinate (xw, yh) of other object lies ín the this.object
-        if (this.x <= otherGameItem.xw &&
-            this.xw >= otherGameItem.xw &&
-            this.y <= otherGameItem.yh &&
-            this.yh >= otherGameItem.yh) {
+        if (this.x <= otherGameItem.xw() &&
+            this.xw() >= otherGameItem.xw() &&
+            this.y <= otherGameItem.yh() &&
+            this.yh() >= otherGameItem.yh()) {
                 return true
-        }
+        };
         return false
-    }
-
+    };
     // do the cornercheck the otherway around (using previous method)
     checkColission(otherGameItem) {
         return this.cornerCheck(otherGameItem) || otherGameItem.cornerCheck(this)
     }
-}
+    // move general
+    move(x, y){
+        this.x +=x
+        this.y +=y
+    }
+    moveVertical(y) {
+        this.move(0, y)
+    }
+    moveHorizontal(x) {
+        this.move(x,0)
+    }    
+};
+
+// exten the ImgItem with the GameItems properties and methods
+class ImgItem extends GamteItem {
+
+};
 
 
 
